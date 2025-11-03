f399be9450144756355d6da7e6e99e6a9d21dd0e
Guiran <150613697+GuiG2023@users.noreply.github.com>
2025-11-03 08:59:27 -0800

    test

--- sobel_cpu.cpp
+++ sobel_cpu.cpp
@@ -100,16 +100,7 @@ void do_sobel_filtering(float *in, float *out, int ncols, int nrows)
    {
       for (int j = 0; j < ncols; j++)
       {
-
-         if (i == 0 || i == nrows - 1 || j == 0 || j == ncols - 1)
-         {
-            out[i * ncols + j] = 0.0;
-         }
-         else
-         {
-
-            out[i * ncols + j] = sobel_filtered_pixel(in, i, j, ncols, nrows, Gx, Gy);
-         }
+         out[i * ncols + j] = sobel_filtered_pixel(in, i, j, ncols, nrows, Gx, Gy);
       }
    }
 }
