cc6874bcb44dd9648df6645c1a9adae9bff81e72
Guiran <150613697+GuiG2023@users.noreply.github.com>
2025-11-03 09:48:10 -0800

    test2

--- sobel_gpu.cu
+++ sobel_gpu.cu
@@ -14,15 +14,14 @@
 
 // see https://en.wikipedia.org/wiki/Sobel_operator
 
-
 // easy-to-find and change variables for the input.
-// specify the name of a file containing data to be read in as bytes, along with 
+// specify the name of a file containing data to be read in as bytes, along with
 // dimensions [columns, rows]
 
 // this is the original laughing zebra image
-//static char input_fname[] = "../data/zebra-gray-int8";
-//static int data_dims[2] = {3556, 2573}; // width=ncols, height=nrows
-//char output_fname[] = "../data/processed-raw-int8-cpu.dat";
+// static char input_fname[] = "../data/zebra-gray-int8";
+// static int data_dims[2] = {3556, 2573}; // width=ncols, height=nrows
+// char output_fname[] = "../data/processed-raw-int8-cpu.dat";
 
 // this one is a 4x augmentation of the laughing zebra
 static char input_fname[] = "../data/zebra-gray-int8-4x";
@@ -31,13 +30,17 @@ char output_fname[] = "../data/processed-raw-int8-4x-cpu.dat";
 
 // see https://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
 // macro to check for cuda errors. basic idea: wrap this macro around every cuda call
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+#define gpuErrchk(ans)                      \
+   {                                        \
+      gpuAssert((ans), __FILE__, __LINE__); \
+   }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true)
 {
    if (code != cudaSuccess)
    {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
+      fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort)
+         exit(code);
    }
 }
 
@@ -56,20 +59,41 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 // see https://en.wikipedia.org/wiki/Sobel_operator
 //
 __device__ float
-sobel_filtered_pixel(float *s, int i, int j , int ncols, int nrows, float *gx, float *gy)
+sobel_filtered_pixel(float *s, int i, int j, int ncols, int nrows, float *gx, float *gy)
 {
 
-   float t=0.0;
+   float t = 0.0;
 
    // ADD CODE HERE:  add your code here for computing the sobel stencil computation at location (i,j)
    // of input s, returning a float
+
+   float Gx = 0.0;
+   float Gy = 0.0;
+
+   int index = 0;
+   for (int di = -1; di <= 1; di++)
+   {
+      for (int dj = -1; dj <= 1; dj++)
+      {
+         int ni = i + di;
+         int nj = j + dj;
+
+         if (ni >= 0 && ni < nrows && nj >= 0 && nj < ncols)
+         {
+            Gx += s[ni * ncols + nj] * gx[index];
+            Gy += s[ni * ncols + nj] * gy[index];
+         }
+         index++;
+      }
+   }
+   t = sqrt(Gx * Gx + Gy * Gy);
 
    return t;
 }
 
 //
 // this function is the kernel that runs on the device
-// 
+//
 // this code will look at CUDA variables: blockIdx, blockDim, threadIdx, blockDim and gridDim
 // to compute the index/stride to use in striding through the source array, calling the
 // sobel_filtered_pixel() function at each location to do the work.
@@ -81,33 +105,47 @@ sobel_filtered_pixel(float *s, int i, int j , int ncols, int nrows, float *gx, f
 // output: float *d - the buffer for the output, size=rows*cols.
 //
 
-
 __global__ void
-sobel_kernel_gpu(float *s,  // source image pixels
-      float *d,  // dst image pixels
-      int n,  // size of image cols*rows,
-      int nrows,
-      int ncols,
-      float *gx, float *gy) // gx and gy are stencil weights for the sobel filter
+sobel_kernel_gpu(float *s, // source image pixels
+                 float *d, // dst image pixels
+                 int n,    // size of image cols*rows,
+                 int nrows,
+                 int ncols,
+                 float *gx, float *gy) // gx and gy are stencil weights for the sobel filter
 {
    // ADD CODE HERE: insert your code here that iterates over every (i,j) of input,  makes a call
    // to sobel_filtered_pixel, and assigns the resulting value at location (i,j) in the output.
 
    // because this is CUDA, you need to use CUDA built-in variables to compute an index and stride
    // your processing motif will be very similar here to that we used for vector add in Lab #2
+
+   // Calculate 1D index and stride
+   int idx = blockIdx.x * blockDim.x + threadIdx.x;
+   int stride = blockDim.x * gridDim.x;
+
+   // Process pixels with grid-stride loop
+   for (int i = idx; i < n; i += stride)
+   {
+      int row = i / ncols;
+      int col = i % ncols;
+
+      if (row < nrows && col < ncols)
+      {
+         d[i] = sobel_filtered_pixel(s, row, col, ncols, nrows, gx, gy);
+      }
+   }
 }
 
-int
-main (int ac, char *av[])
+int main(int ac, char *av[])
 {
    // input, output file names hard coded at top of file
 
    // load the input file
-   off_t nvalues = data_dims[0]*data_dims[1];
-   unsigned char *in_data_bytes = (unsigned char *)malloc(sizeof(unsigned char)*nvalues);
+   off_t nvalues = data_dims[0] * data_dims[1];
+   unsigned char *in_data_bytes = (unsigned char *)malloc(sizeof(unsigned char) * nvalues);
 
-   FILE *f = fopen(input_fname,"r");
-   if (fread((void *)in_data_bytes, sizeof(unsigned char), nvalues, f) != nvalues*sizeof(unsigned char))
+   FILE *f = fopen(input_fname, "r");
+   if (fread((void *)in_data_bytes, sizeof(unsigned char), nvalues, f) != nvalues * sizeof(unsigned char))
    {
       printf("Error reading input file. \n");
       fclose(f);
@@ -121,61 +159,79 @@ main (int ac, char *av[])
 
    // now convert input from byte, in range 0..255, to float, in range 0..1
    float *in_data_floats;
-   gpuErrchk( cudaMallocManaged(&in_data_floats, sizeof(float)*nvalues) );
+   gpuErrchk(cudaMallocManaged(&in_data_floats, sizeof(float) * nvalues));
 
-   for (off_t i=0; i<nvalues; i++)
+   for (off_t i = 0; i < nvalues; i++)
       in_data_floats[i] = (float)in_data_bytes[i] * ONE_OVER_255;
 
    // now, create a buffer for output
    float *out_data_floats;
-   gpuErrchk( cudaMallocManaged(&out_data_floats, sizeof(float)*nvalues) );
-   for (int i=0;i<nvalues;i++)
-      out_data_floats[i] = 1.0;  // assign "white" to all output values for debug
+   gpuErrchk(cudaMallocManaged(&out_data_floats, sizeof(float) * nvalues));
+   for (int i = 0; i < nvalues; i++)
+      out_data_floats[i] = 1.0; // assign "white" to all output values for debug
 
    // define sobel filter weights, copy to a device accessible buffer
    float Gx[9] = {1.0, 0.0, -1.0, 2.0, 0.0, -2.0, 1.0, 0.0, -1.0};
    float Gy[9] = {1.0, 2.0, 1.0, 0.0, 0.0, 0.0, -1.0, -2.0, -1.0};
    float *device_gx, *device_gy;
-   gpuErrchk( cudaMallocManaged(&device_gx, sizeof(float)*sizeof(Gx)) );
-   gpuErrchk( cudaMallocManaged(&device_gy, sizeof(float)*sizeof(Gy)) );
+   gpuErrchk(cudaMallocManaged(&device_gx, sizeof(float) * sizeof(Gx)));
+   gpuErrchk(cudaMallocManaged(&device_gy, sizeof(float) * sizeof(Gy)));
 
-   for (int i=0;i<9;i++) // copy from Gx/Gy to device_gx/device_gy
+   for (int i = 0; i < 9; i++) // copy from Gx/Gy to device_gx/device_gy
    {
       device_gx[i] = Gx[i];
       device_gy[i] = Gy[i];
    }
-   
+
    // now, induce memory movement to the GPU of the data in unified memory buffers
 
-   int deviceID=0; // assume GPU#0, always. OK assumption for this program
-   cudaMemPrefetchAsync((void *)in_data_floats, nvalues*sizeof(float), deviceID);
-   cudaMemPrefetchAsync((void *)out_data_floats, nvalues*sizeof(float), deviceID);
-   cudaMemPrefetchAsync((void *)device_gx, sizeof(Gx)*sizeof(float), deviceID);
-   cudaMemPrefetchAsync((void *)device_gy, sizeof(Gy)*sizeof(float), deviceID);
+   int deviceID = 0; // assume GPU#0, always. OK assumption for this program
+   cudaMemPrefetchAsync((void *)in_data_floats, nvalues * sizeof(float), deviceID);
+   cudaMemPrefetchAsync((void *)out_data_floats, nvalues * sizeof(float), deviceID);
+   cudaMemPrefetchAsync((void *)device_gx, sizeof(Gx) * sizeof(float), deviceID);
+   cudaMemPrefetchAsync((void *)device_gy, sizeof(Gy) * sizeof(float), deviceID);
 
    // set up to run the kernel
-   int nBlocks=1, nThreadsPerBlock=256;
+   int nBlocks = 1, nThreadsPerBlock = 256;
 
    // ADD CODE HERE: insert your code here to set a different number of thread blocks or # of threads per block
+   // Check if command line arguments are provided
+   if (ac == 3)
+   {
+      nBlocks = atoi(av[1]);
+      nThreadsPerBlock = atoi(av[2]);
+   }
+   else if (ac != 1)
+   {
+      printf("Usage: %s [num_blocks num_threads_per_block]\n", av[0]);
+      printf("Example: %s 128 256\n", av[0]);
+      return 1;
+   }
 
+   // Calculate total number of threads and validate
 
-
+   int totalThreads = nBlocks * nThreadsPerBlock;
+   if (totalThreads <= 0)
+   {
+      printf("Error: Invalid thread configuration...\n");
+      return 1;
+   }
    printf(" GPU configuration: %d blocks, %d threads per block \n", nBlocks, nThreadsPerBlock);
 
    // invoke the kernel on the device
    sobel_kernel_gpu<<<nBlocks, nThreadsPerBlock>>>(in_data_floats, out_data_floats, nvalues, data_dims[1], data_dims[0], device_gx, device_gy);
 
    // wait for it to finish, check errors
-   gpuErrchk (  cudaDeviceSynchronize() );
+   gpuErrchk(cudaDeviceSynchronize());
 
    // write output after converting from floats in range 0..1 to bytes in range 0..255
-   unsigned char *out_data_bytes = in_data_bytes;  // just reuse the buffer from before
-   for (off_t i=0; i<nvalues; i++)
+   unsigned char *out_data_bytes = in_data_bytes; // just reuse the buffer from before
+   for (off_t i = 0; i < nvalues; i++)
       out_data_bytes[i] = (unsigned char)(out_data_floats[i] * 255.0);
 
-   f = fopen(output_fname,"w");
+   f = fopen(output_fname, "w");
 
-   if (fwrite((void *)out_data_bytes, sizeof(unsigned char), nvalues, f) != nvalues*sizeof(unsigned char))
+   if (fwrite((void *)out_data_bytes, sizeof(unsigned char), nvalues, f) != nvalues * sizeof(unsigned char))
    {
       printf("Error writing output file. \n");
       fclose(f);
