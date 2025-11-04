15fc1347c51b7ac0efb3301c7e660c5b273e4224
Guiran <150613697+GuiG2023@users.noreply.github.com>
2025-11-03 17:27:30 -0800

    debug 3

--- sobel_cpu_omp_offload.cpp
+++ sobel_cpu_omp_offload.cpp
@@ -116,7 +116,7 @@ void do_sobel_filtering(float *in, float *out, int ncols, int nrows)
       // ADD CODE HERE: insert your code here that iterates over every (i,j) of input,  makes a call
       // to sobel_filtered_pixel, and assigns the resulting value at location (i,j) in the output.
 
-#pragma omp target teams distribute parallel for collapse(2)
+#pragma omp teams distribute parallel for collapse(2)
       for (int i = 0; i < height; i++)
       {
          for (int j = 0; j < width; j++)
